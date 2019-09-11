4d79a8c49c2e5c68bbe7d60c2b060a327ea1a11a
Lucas Godoy <lcsdegodoy@outlook.com>
2019-09-11 20:52:48 -0300

    feat(player): spawn player on connect

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,2 +1,9 @@
 import alt from 'alt';
-alt.log('server initialized');
+
+/**
+ * This event is triggered when a player joins the server
+ * @param {Player} player - The player who joined the server
+ */
+alt.on('playerConnect', (player: alt.Player) => {
+  player.spawn(0, 0, 72, 0);
+});
